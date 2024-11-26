f43f995f62ceea9dbfa9b940641126b07e277799
Rakibul73 <rakibul29185@yahoo.com>
2024-11-26 19:07:26 +0600

    used another port

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3001);
 }
 bootstrap();
